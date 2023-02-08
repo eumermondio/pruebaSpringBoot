c3875312ac7c486dc98464e25c8dd4a019273923
eumermondio <cpachecoarjona@gmail.com>
2023-02-08 11:26:15 +0100

    hecho todo

--- springPrueba/src/main/java/com/example/demo/controllers/UsuariaController.java
+++ springPrueba/src/main/java/com/example/demo/controllers/UsuariaController.java
@@ -10,9 +10,11 @@ import javax.xml.bind.DatatypeConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,28 +39,42 @@ public class UsuariaController {
 		List<Usuaria> usrs = usuRep.findAll();
 		for (Usuaria u : usrs) {
 			DTO dto = new DTO();
+			dto.put("id", u.getId());
+			if (u.getFechaElim() != null) {
+				dto.put("fecha_elim", u.getFechaElim().toString());
+			}
 			dto.put("nombre", u.getNombre());
-			dto.put("fecha_nac", u.getFechaNac().toString());
+			if (u.getFechaNac() != null) {
+				dto.put("fecha_nac", u.getFechaNac().toString());
+			}
+			dto.put("imagen", u.getImg());
 			dto.put("pass", u.getPass());
 			dto.put("username", u.getUsername());
+			if (u.getUsuarioTipo() != null) {
+				dto.put("id_tipo_usuario", u.getUsuarioTipo().getId());
+			}
 			usuarios.add(dto);
 		}
 		return usuarios;
 	}
 
-	@GetMapping("/obtener/{id}")
+	@GetMapping("/usuario/{id}")
 	public DTO getUsuario(@PathVariable("id") int id) {
 		Usuaria u = usuRep.findById(id);
 		DTO dto = new DTO();
-		dto.put("nombre", u.getNombre());
-		dto.put("fecha_nac", u.getFechaNac().toString());
-		dto.put("pass", u.getPass());
-		dto.put("username", u.getUsername());
+		if (u != null) {
+			dto.put("nombre", u.getNombre());
+			dto.put("fecha_nac", u.getFechaNac().toString());
+			dto.put("pass", u.getPass());
+			dto.put("username", u.getUsername());
+		} else {
+			dto.put("error", "not found");
+		}
 		return dto;
 	}
 
 	@PostMapping(path = "/crear", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void autenticaUsuario(@RequestBody DatosAltaUsuario u, HttpServletRequest request) {
+	public void cambiaUsuario(@RequestBody DatosAltaUsuario u, HttpServletRequest request) {
 		usuRep.save(new Usuaria(u.id, null, u.fechaNac, DatatypeConverter.parseBase64Binary(u.img), u.nombre, u.pass,
 				u.username, usuTipo.findById(u.rol)));
 	}
@@ -87,4 +103,21 @@ public class UsuariaController {
 			this.rol = rol;
 		}
 	}
+
+	@PutMapping(path = "/usuario/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void autenticaUsuario(@RequestBody DatosAltaUsuario u, HttpServletRequest request,
+			@PathVariable("id") int id) {
+		Usuaria usuarioAct = new Usuaria(id, null, u.fechaNac, DatatypeConverter.parseBase64Binary(u.img), u.nombre,
+				u.pass, u.username, usuTipo.findById(u.rol));
+		usuRep.save(usuarioAct);
+	}
+
+	@DeleteMapping("/usuario/{id}")
+	public void borrarUsuario(@PathVariable("id") int id) {
+		Usuaria u = usuRep.findById(id);
+		if (u != null) {
+			usuRep.deleteById(id);
+		}
+	}
+
 }
